7d34039a06fb3e3e7e7392f82b96ff0ac356dfec
fcventura02 <filipecvr4@gmail.com>
2021-09-20 13:22:15 -0300

    updated style card drink

--- scripts/renderDrinksHome.js
+++ scripts/renderDrinksHome.js
@@ -1,19 +1,24 @@
 import { randomDrinks } from "./drinksApi.js";
 const containerDrinks = document.getElementById("container-drinks");
 
-const createCard = (name, img) => {
+const createCard = (id, name, img, alcoholic) => {
   const cardDrink = document.createElement("div");
   cardDrink.setAttribute("class", "card-drink");
   const drinkInfo = document.createElement("div");
-  drinkInfo.setAttribute("class", "drink-name");
+  drinkInfo.setAttribute("class", "drink-info");
   const drinkImage = document.createElement("img");
   drinkImage.setAttribute("class", "drink-img");
   drinkImage.setAttribute("src", img);
   drinkImage.setAttribute("alt", "Drink image");
+  const drinkAlcoholic = document.createElement("p");
   const drinkName = document.createElement("p");
   drinkName.innerText = name;
-
+  drinkAlcoholic.innerText = alcoholic;
+  alcoholic === "Alcoholic"
+    ? drinkAlcoholic.setAttribute("class", "alcoholic")
+    : drinkAlcoholic.setAttribute("class", "no-alcoholic");
   drinkInfo.appendChild(drinkName);
+  drinkInfo.appendChild(drinkAlcoholic);
   cardDrink.appendChild(drinkImage);
   cardDrink.appendChild(drinkInfo);
   return cardDrink;
@@ -30,14 +35,9 @@ Promise.all([
   .then((res) => {
     for (const iterator of res) {
       const { idDrink, strAlcoholic, strDrink, strDrinkThumb } = iterator;
-      containerDrinks.appendChild(createCard(strDrink, strDrinkThumb));
+      containerDrinks.appendChild(
+        createCard(idDrink, strDrink, strDrinkThumb, strAlcoholic)
+      );
     }
   })
-  .then((res) => document.getElementById("loader").style.display = "none");
-
-/* for (let i = 0; i < 6; i++) {
-  randomDrinks().then((res) => {
-    const { idDrink, strAlcoholic, strDrink, strDrinkThumb } = res;
-    containerDrinks.appendChild(createCard(strDrink, strDrinkThumb));
-  });
-} */
+  .then((res) => (document.getElementById("loader").style.display = "none"));
