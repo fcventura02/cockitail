e9c56b0f454afba57d913c0ca8122ff57c712944
fcventura02 <filipecvr4@gmail.com>
2021-09-21 13:41:01 -0300

    Updated menu button, removed scroll on activated

--- scripts/menu-btn.js
+++ scripts/menu-btn.js
@@ -1,7 +1,14 @@
 const hamburguerBtn = document.getElementById("hamburguer-btn");
 const menu = document.getElementById("menu");
 
-hamburguerBtn.addEventListener("click", ()=>{
-	menu.classList.toggle("activit")
-	menu.classList.toggle("not-activit")
-})
+hamburguerBtn.addEventListener("click", () => {
+  menu.classList.toggle("activit");
+  menu.classList.toggle("not-activit");
+  console.log(menu.className === "activit");
+
+  menu.className === "activit"
+    ? (window.onscroll = function () {
+        window.scrollTo(0, 0);
+      })
+    : (window.onscroll = () => {});
+});
