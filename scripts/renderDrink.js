ab8986e1927c52f06c8df33fe943fc8460d446ef
fcventura02 <filipecvr4@gmail.com>
2021-09-21 13:45:03 -0300

    Created render drink

---
+++ scripts/renderDrink.js
@@ -0,0 +1,50 @@
+import { getDrink } from "./drinksApi.js";
+
+const nameDrink = document.getElementById("title");
+const typeCategory = document.getElementById("category");
+const typeGlass = document.getElementById("glass");
+const instructions = document.getElementById("instructions");
+const imgDrinkThumb = document.getElementById("thumbnail");
+const tableIngredientes = document.getElementById("ingredients");
+
+const renderDrink = (arrDrink) => {
+  const {
+    strDrink,
+    strDrinkThumb,
+    strCategory,
+    strGlass,
+    strInstructions,
+    ...itens
+  } = arrDrink[0];
+
+  imgDrinkThumb.setAttribute("src", strDrinkThumb);
+  typeCategory.innerText = strCategory;
+  typeGlass.innerText = strGlass;
+  nameDrink.innerText = strDrink;
+  instructions.innerText = strInstructions;
+
+  for (let i = 1; i <= 15; i++) {
+    const tr = document.createElement("tr");
+    const tdIngrediente = document.createElement("td");
+    const tdMedida = document.createElement("td");
+    tr.appendChild(tdIngrediente);
+    tr.appendChild(tdMedida);
+    if (itens[`strIngredient${i}`] === null && itens[`strMeasure${i}`] === null)
+      break;
+    tdIngrediente.innerText = itens[`strIngredient${i}`];
+    tdMedida.innerText = itens[`strMeasure${i}`];
+    tableIngredientes.appendChild(tr);
+  }
+};
+
+const drink = () => {
+  const id = window.location.search.replace("?", "");
+  getDrink(id).then((res) => {
+    renderDrink(res);
+  });
+};
+
+drink();
+
+/* Ingredientes do Drink (strIngredient1 até strIngrediente15, tratar respostas 'null')
+Quantidade de cada Ingrediente (strMeasure1 até strMeasure15, tratar respostas 'null') */
