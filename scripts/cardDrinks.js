1bec85f54e2acd6fffcb1f017c2bcd9dd3588e67
fcventura02 <filipecvr4@gmail.com>
2021-09-23 13:35:51 -0300

    added paginate

--- scripts/cardDrinks.js
+++ scripts/cardDrinks.js
@@ -1,3 +1,5 @@
+const containerPaginate = document.getElementById("container-paginate");
+
 const createCard = (id, name, img, alcoholic) => {
   const cardDrink = document.createElement("a");
   cardDrink.setAttribute("class", "card-drink");
@@ -9,28 +11,54 @@ const createCard = (id, name, img, alcoholic) => {
   drinkImage.setAttribute("class", "drink-img");
   drinkImage.setAttribute("src", img);
   drinkImage.setAttribute("alt", "Drink image");
-  const drinkAlcoholic = document.createElement("p");
+  let drinkAlcoholic;
+  if (alcoholic !== undefined) {
+    drinkAlcoholic = document.createElement("p");
+    drinkAlcoholic.innerText = alcoholic;
+    alcoholic === "Alcoholic"
+      ? drinkAlcoholic.setAttribute("class", "alcoholic")
+      : drinkAlcoholic.setAttribute("class", "no-alcoholic");
+    drinkInfo.appendChild(drinkAlcoholic);
+  }
   const drinkName = document.createElement("p");
   drinkName.innerText = name;
-  drinkAlcoholic.innerText = alcoholic;
-  alcoholic === "Alcoholic"
-    ? drinkAlcoholic.setAttribute("class", "alcoholic")
-    : drinkAlcoholic.setAttribute("class", "no-alcoholic");
   drinkInfo.appendChild(drinkName);
-  drinkInfo.appendChild(drinkAlcoholic);
   cardDrink.appendChild(drinkImage);
   cardDrink.appendChild(drinkInfo);
   return cardDrink;
 };
 
-export const renderDrinks = (arrDrinks, element) => {
-	element.innerHTML = "";
+const listDrinks = (arrDrinks, element) => {
+  element.innerHTML = "";
   for (const iterator of arrDrinks) {
     const { idDrink, strAlcoholic, strDrink, strDrinkThumb } = iterator;
-    const thumb = strDrinkThumb.concat("/preview")
-    console.log
-    element.appendChild(
-      createCard(idDrink, strDrink, thumb, strAlcoholic)
-    );
+    const thumb = strDrinkThumb.concat("/preview");
+    element.appendChild(createCard(idDrink, strDrink, thumb, strAlcoholic));
   }
+};
+
+const paginateDrinks = (pages, arrDrinks, element) => {
+  const div = document.createElement("div");
+  div.setAttribute("class", "list-pages");
+  for (let index = 1; index <= pages; index++) {
+    const page = document.createElement("button");
+    page.setAttribute("class", index === 1 ? "page-active" : "page-not-active");
+    page.setAttribute("id", index);
+    page.addEventListener("click", () => {
+      document.getElementsByClassName("page-active")[0].className =
+        "page-not-active";
+      page.className = "page-active";
+      listDrinks(arrDrinks.slice((index - 1) * 9, index * 9), element);
+    });
+    page.innerText = index;
+    div.appendChild(page);
+  }
+  containerPaginate.appendChild(div);
+};
+
+export const renderDrinks = (arrDrinks, element) => {
+  const pages = Math.ceil(arrDrinks.length / 9);
+  if (containerPaginate) containerPaginate.innerHTML = "";
+  pages > 1 && paginateDrinks(pages, arrDrinks, element);
+  listDrinks(arrDrinks.slice(0, 9), element);
 };
